0320d5ffc75416b89344c0f32528b2072bc73a6e
Tin <tin@Tins-MacBook-Pro.local>
2026-06-08 14:57:53 -0700

    update Map

--- ParkGo/Services/Map/MapService.swift
+++ ParkGo/Services/Map/MapService.swift
@@ -3,21 +3,24 @@ import MapKit
 
 final class MapService {
     func openInAppleMaps(for lot: ParkingLot) {
-        let placemark = MKPlacemark(coordinate: lot.coordinate)
-        let item = MKMapItem(placemark: placemark)
-        item.name = lot.name
-        item.openInMaps(launchOptions: [
-            MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
-        ])
+            let placemark = MKPlacemark(coordinate: lot.coordinate)
+            let item = MKMapItem(placemark: placemark)
+            
+            item.name = lot.name
+            item.openInMaps(launchOptions: [
+                MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+            ])
+        }
+        
+        func openCoordinateInAppleMaps(latitude: Double, longitude: Double, name: String) {
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let placemark = MKPlacemark(coordinate: coordinate)
+            let item = MKMapItem(placemark: placemark)
+            
+            item.name = name
+            item.openInMaps(launchOptions: [
+                MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
+            ])
+        }
     }
-
-    func openCoordinateInAppleMaps(latitude: Double, longitude: Double, name: String) {
-        let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let item = MKMapItem(placemark: MKPlacemark(coordinate: coordinate))
-        item.name = name
-        item.openInMaps(launchOptions: [
-            MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving
-        ])
-    }
-}
 
